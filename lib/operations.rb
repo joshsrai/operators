b6647e07f8a507bbcdd066c4d442928bbce5ec14
joshsrai <joshsrai@gmail.com>
2020-11-13 15:10:11 -0500

    commit tree c29d6b7bec76b2d64fb99a5cb0ea5d548c336396

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,19 @@
 def unsafe?(speed)
-
+    if speed > 60 #if the speed is greater than 60, return 'true', unsafe
+        true
+    elsif speed < 40 #if the speed is less than 40, return 'true', unsafe
+        true
+    else
+        false
+    end
 end
 
 
 
 def not_safe?(speed)
-	
+    speed > 60 || speed < 40 ? true : false
+    #if the speed is greater than 60 or if the speed is less than 40 then return 'true'
+    #if the speed is less than 60 or if the speed is greater than 40 then return 'false'
 end
 	
 
